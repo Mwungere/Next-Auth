7673234749e8494f986e06981bbc3419305510fe
Mwungere <mwungereelite56@gmail.com>
2025-01-30 22:05:08 +0200

    setting up prisma

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,40 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod"
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
 
-  if(!validatedFields) {
+  if(!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!"}
+  const { email, password, name } = validatedFields.data;
+  
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await db.user.findUnique({
+    where: { email }
+  })
+
+  if(existingUser) {
+    return { error: "Email already in use!" }
+  }
+
+  await db.user.create({
+    data: { 
+      name,
+      email, 
+      password: hashedPassword, 
+    }
+  })
+
+  // TODO: Send verification email
+ 
+  return { success: "User created successfully!"}
 }
+ 
+
+//2:01:27
