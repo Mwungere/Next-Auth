34b827dcb8bc6106110eee8775c84813cca7f25e
Mwungere <mwungereelite56@gmail.com>
2025-01-31 12:11:22 +0200

    commiting

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod"
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -15,9 +16,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const { email, password, name } = validatedFields.data;
   
   const hashedPassword = await bcrypt.hash(password, 10);
-  const existingUser = await db.user.findUnique({
-    where: { email }
-  })
+  const existingUser = await getUserByEmail(email);
 
   if(existingUser) {
     return { error: "Email already in use!" }
@@ -37,4 +36,3 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 }
  
 
-//2:01:27
