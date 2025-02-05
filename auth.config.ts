22f2db682122041539c4d397c90be538b4ff3ffb
Mwungere <mwungereelite56@gmail.com>
2025-02-05 11:05:19 +0200

    callbacks

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,12 @@
 import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import Github from "next-auth/providers/github"
 import { LoginSchema } from "@/schemas"
 import { getUserByEmail } from "./data/user"
  
 export default { providers: [
+    Github,
     Credentials({
         async authorize(credentials) {
             const validatedFields = LoginSchema.safeParse(credentials)
