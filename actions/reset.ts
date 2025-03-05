284686fd815714480eaabf6647c93fe3741e6fd6
Mwungere <mwungereelite56@gmail.com>
2025-03-05 15:38:00 +0200

    updating password reset

--- actions/reset.ts
+++ actions/reset.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
+import { sendPasswordResetEmail } from "@/lib/mail";
+import { generatePasswordResetToken } from "@/lib/tokens";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
 
@@ -21,7 +23,13 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
         return { error: "Email not found!"};
     }
 
-    // TODO: Generate token and send email
+
+    const passwordResetToken = await generatePasswordResetToken(email)
+
+    await sendPasswordResetEmail(
+        passwordResetToken.email,
+        passwordResetToken.token
+    )
 
     return {success: "Reset email sent!"};
 }
