e2569fbf14b29729734326ed6ee28ec923e6e2da
Mwungere <mwungereelite56@gmail.com>
2025-03-08 10:34:08 +0200

    updating 2FA

--- data/two-factor-token.ts
+++ data/two-factor-token.ts
@@ -15,3 +15,16 @@ export const getTwoFactorTokenByToken = async (token: string) => {
         return null;
     }
 }
+export const getTwoFactorTokenByEmail = async (email: string) => {
+    try {
+
+        const twoFactorToken = await db.twoFactorToken.findFirst({
+            where: {email}
+        })
+
+        return twoFactorToken;
+        
+    } catch {
+        return null;
+    }
+}
