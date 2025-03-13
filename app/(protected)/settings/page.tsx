6da3e7924b885fd80ac8043a040b661d5fb5e8b5
Mwungere <mwungereelite56@gmail.com>
2025-03-13 12:08:05 +0200

    commiting

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,46 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useSession } from "next-auth/react";
-import React, { useTransition } from "react";
+import React, { useTransition, useState } from "react";
+
+import { SettingsSchema } from "@/schemas";
+import { Form, FormField } from "@/components/ui/form";
+
 
 const Settings = () => {
   const { update } = useSession()
   const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
 
-  const onClick = () => {
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+ 
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
+
     startTransition(() => {
-      settings({
-        name: "Something different!",
-      })
-      .then(() => {
-        update();
+      settings(values)
+      .then((data) => {
+
+        if(data.error){
+          setError(data.error);
+        }
+
+        if(data.success){
+          setSuccess(data.success);
+          update();
+        }
+
       });
     });
   };
@@ -27,7 +51,11 @@ const Settings = () => {
         <p className="text-2xl font-semibold text-center">⚙️ Settings</p>
       </CardHeader>
       <CardContent>
-        <Button disabled={isPending} onClick={onClick}>Update name</Button>
+        <Form {...form}>
+          <form className="space-y-6" onClick={form.handleSubmit(onSubmit)}>
+            {/* <FormField/> */}
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { getUserById } from "@/data/user";
 import { UserRole } from "@prisma/client";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountById } from "@/data/account";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -69,6 +70,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       if(session.user){
         session.user.name = token.name;
         session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
 
       return session;
@@ -81,6 +83,10 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
+
+      const existingAccount = await getAccountById(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
 
       token.name = existingUser.name;
       token.email = existingUser.email;
