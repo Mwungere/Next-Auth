2849b27c11f1f6c6f4d586f9e0836110c28883d0
Mwungere <mwungereelite56@gmail.com>
2025-03-13 14:47:44 +0200

    settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -10,11 +10,23 @@ import { useSession } from "next-auth/react";
 import React, { useTransition, useState } from "react";
 
 import { SettingsSchema } from "@/schemas";
-import { Form, FormField } from "@/components/ui/form";
-
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { useCurrentuser } from "@/hooks/use-current-user";
+import { Input } from "@/components/ui/input";
+import { FormSuccess } from "@/components/form-success";
+import { FormError } from "@/components/form-error";
 
 const Settings = () => {
-  const { update } = useSession()
+
+  const user = useCurrentuser();
+
+  const { update } = useSession();
   const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
@@ -22,25 +34,21 @@ const Settings = () => {
   const form = useForm<z.infer<typeof SettingsSchema>>({
     resolver: zodResolver(SettingsSchema),
     defaultValues: {
-      name: "",
+      name: user?.name || undefined,
     },
   });
- 
+
   const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
-
     startTransition(() => {
-      settings(values)
-      .then((data) => {
-
-        if(data.error){
+      settings(values).then((data) => {
+        if (data.error) {
           setError(data.error);
         }
 
-        if(data.success){
+        if (data.success) {
           setSuccess(data.success);
           update();
         }
-
       });
     });
   };
@@ -52,8 +60,33 @@ const Settings = () => {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form className="space-y-6" onClick={form.handleSubmit(onSubmit)}>
-            {/* <FormField/> */}
+          <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="text"
+                        placeholder="John Doe"
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormSuccess message={success} />
+            <FormError message={error} />
+
+            <Button type="submit" disabled={isPending}>
+              Save
+            </Button>
           </form>
         </Form>
       </CardContent>
